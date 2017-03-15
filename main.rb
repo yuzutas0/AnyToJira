e87db8e6489df8352562f6712ccaddb83a8a405a
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-15 16:37:04 +0900

    access jira

--- main.rb
+++ main.rb
@@ -5,40 +5,59 @@ Dotenv.load '.env'
 
 require 'mechanize'
 require 'open-uri'
+require 'oauth'
+require 'jira-ruby'
 
 # -----------------------------------------------
 # Confluence -> JIRA
 # -----------------------------------------------
 
+options = {
+    :username        => ENV['JIRA_MAILADDRESS'],
+    :password        => ENV['JIRA_PASSWORD'],
+    :site            => ENV['JIRA_HOST'],
+    :context_path    => '/jira',
+    :use_ssl         => false,
+    :auth_type       => :basic
+}
+
+client = JIRA::Client.new(options)
+project = client.Project.find('KOIEN')
+
+project.issues.each do |issue|
+  puts "#{issue.id} - #{issue.summary}"
+end
+
+
 agent = Mechanize.new
 agent.user_agent = 'Mac Safari'
 
-JIRA_XPATH_PREFIX = '//*[@id="main-content"]/div/table/tbody/tr['
-JIRA_XPATH_SUFFIX = ']/td[' + ENV['JIRA_TABLE_COLUMN'].to_i.to_s + ']'
+CONFLUENCE_XPATH_PREFIX = '//*[@id="main-content"]/div/table/tbody/tr['
+CONFLUENCE_XPATH_SUFFIX = ']/td[' + ENV['CONFLUENCE_TABLE_COLUMN'].to_i.to_s + ']'
 
-agent.get(ENV['JIRA_HOST'] + 'login.action') do |page|
+agent.get(ENV['CONFLUENCE_HOST'] + 'login.action') do |page|
   page.form_with(:name => 'loginform') do |form|
     form.os_username = ENV['JIRA_MAILADDRESS']
     form.os_password = ENV['JIRA_PASSWORD']
   end.submit
 
-  html = agent.get(ENV['JIRA_HOST'] + 'pages/viewpage.action?pageId=' + ENV['JIRA_PAGE']).content.toutf8
+  html = agent.get(ENV['CONFLUENCE_HOST'] + 'pages/viewpage.action?pageId=' + ENV['CONFLUENCE_PAGE']).content.toutf8
   contents = Nokogiri::HTML(html, nil, 'utf-8')
 
   result = []
-  ENV['JIRA_TABLE_START_ROW'].to_i.upto(ENV['JIRA_TABLE_END_ROW'].to_i) do |index|
-    jira_xpath = JIRA_XPATH_PREFIX + index.to_s + JIRA_XPATH_SUFFIX
-    break if contents.xpath(jira_xpath).empty?
+  ENV['CONFLUENCE_TABLE_START_ROW'].to_i.upto(ENV['CONFLUENCE_TABLE_END_ROW'].to_i) do |index|
+    confluence_xpath = CONFLUENCE_XPATH_PREFIX + index.to_s + CONFLUENCE_XPATH_SUFFIX
+    break if contents.xpath(confluence_xpath).empty?
 
-    unless contents.xpath(jira_xpath).xpath('.//li').empty?
-      list_word = contents.xpath(jira_xpath).xpath('.//li').text.to_s
+    unless contents.xpath(confluence_xpath).xpath('.//li').empty?
+      list_word = contents.xpath(confluence_xpath).xpath('.//li').text.to_s
       list_word = list_word[0..4] if list_word.size > 5
-      text = contents.xpath(jira_xpath).text.to_s
+      text = contents.xpath(confluence_xpath).text.to_s
       result << text.split(list_word)[0]
       next
     end
 
-    text = contents.xpath(jira_xpath).text.to_s
+    text = contents.xpath(confluence_xpath).text.to_s
     result << text unless text.empty?
   end
   puts result
