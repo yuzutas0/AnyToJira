24eef0c321f0c798e732718a0eb0e14c8cff42dd
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-15 13:23:16 +0900

    confluence login

--- main.rb
+++ main.rb
@@ -1,6 +1,22 @@
 # encoding:utf-8
 
+require 'mechanize'
+
 # TODO: Confluence -> JIRA
+
+agent = Mechanize.new
+agent.user_agent = 'Mac Safari'
+agent.get('host/login.action') do |page|
+  page.form_with(:name => 'loginform') do |form|
+    form.os_username = 'mail'
+    form.os_password = 'password'
+  end.submit
+
+  agent.get('host/pages/viewpage.action?pageId=xxx') do |page|
+    puts page.inspect
+  end
+end
+
 # TODO: JIRA -> JIRA
 # TODO: Github Issue -> JIRA
 # TODO: TODO/FIXME in Code -> JIRA
