0bb87c9f13fd817f6f8677ebe12776ce1874639f
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-15 16:16:12 +0900

    get confluence table content

--- main.rb
+++ main.rb
@@ -6,18 +6,42 @@ Dotenv.load '.env'
 require 'mechanize'
 require 'open-uri'
 
-# TODO: Confluence -> JIRA
+# -----------------------------------------------
+# Confluence -> JIRA
+# -----------------------------------------------
 
 agent = Mechanize.new
 agent.user_agent = 'Mac Safari'
+
+JIRA_XPATH_PREFIX = '//*[@id="main-content"]/div/table/tbody/tr['
+JIRA_XPATH_SUFFIX = ']/td[' + ENV['JIRA_TABLE_COLUMN'].to_i.to_s + ']'
+
 agent.get(ENV['JIRA_HOST'] + 'login.action') do |page|
   page.form_with(:name => 'loginform') do |form|
     form.os_username = ENV['JIRA_MAILADDRESS']
     form.os_password = ENV['JIRA_PASSWORD']
   end.submit
 
-  content = agent.get(ENV['JIRA_HOST'] + 'pages/viewpage.action?pageId=' + ENV['JIRA_PAGE']).content.toutf8
-  puts content
+  html = agent.get(ENV['JIRA_HOST'] + 'pages/viewpage.action?pageId=' + ENV['JIRA_PAGE']).content.toutf8
+  contents = Nokogiri::HTML(html, nil, 'utf-8')
+
+  result = []
+  ENV['JIRA_TABLE_START_ROW'].to_i.upto(ENV['JIRA_TABLE_END_ROW'].to_i) do |index|
+    jira_xpath = JIRA_XPATH_PREFIX + index.to_s + JIRA_XPATH_SUFFIX
+    break if contents.xpath(jira_xpath).empty?
+
+    unless contents.xpath(jira_xpath).xpath('.//li').empty?
+      list_word = contents.xpath(jira_xpath).xpath('.//li').text.to_s
+      list_word = list_word[0..4] if list_word.size > 5
+      text = contents.xpath(jira_xpath).text.to_s
+      result << text.split(list_word)[0]
+      next
+    end
+
+    text = contents.xpath(jira_xpath).text.to_s
+    result << text unless text.empty?
+  end
+  puts result
 end
 
 # TODO: JIRA -> JIRA
