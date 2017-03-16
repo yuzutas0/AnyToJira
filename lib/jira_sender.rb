6ef1b17891bae4669254970f8fe73f9e1b642cb4
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-16 13:26:30 +0900

    add environment variables

--- lib/jira_sender.rb
+++ lib/jira_sender.rb
@@ -19,7 +19,7 @@ class JIRA_SENDER
         :username        => ENV['JIRA_MAILADDRESS'],
         :password        => ENV['JIRA_PASSWORD'],
         :site            => ENV['JIRA_HOST'],
-        :context_path    => '/jira',
+        :context_path    => ENV['JIRA_CONTEXT_PATH'],
         :use_ssl         => false,
         :auth_type       => :basic
     }
