bd4277453e2ccb254da48e4267762f54a97755c2
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-16 12:54:56 +0900

    divide classes

--- main.rb
+++ main.rb
@@ -1,61 +1,20 @@
 # encoding:utf-8
 
+# -----------------------------------------------
+# Require
+# -----------------------------------------------
 require 'dotenv'
 Dotenv.load '.env'
 
 require 'mechanize'
 require 'open-uri'
 require 'oauth'
-require 'jira-ruby'
-
 
 # -----------------------------------------------
 # XXX -> JIRA
 # -----------------------------------------------
-
-class JIRA_SENDER
-  attr_accessor :client, :project_id, :issue_type
-
-  def initialize
-    options = {
-        :username        => ENV['JIRA_MAILADDRESS'],
-        :password        => ENV['JIRA_PASSWORD'],
-        :site            => ENV['JIRA_HOST'],
-        :context_path    => '/jira',
-        :use_ssl         => false,
-        :auth_type       => :basic
-    }
-
-    @client = JIRA::Client.new(options)
-    @project_id = @client.Project.find(ENV['JIRA_PROJECT_NAME']).id
-    @issue_type = @client.Issuetype.all.find { |type| type.name == ENV['JIRA_ISSUE_NAME'] }.id
-  end
-
-  def send_jira(summary, description)
-    issue = @client.Issue.build
-    response = issue.save(
-        {
-            fields: {
-                summary: summary,
-                description: "Created by #{ENV['JIRA_MAILADDRESS']} Script 'AnyToJira'\n\n#{description}",
-                labels: [
-                    ENV['JIRA_LABEL']
-                ],
-                project: {
-                    id: @project_id
-                },
-                issuetype: {
-                    id: @issue_type
-                }
-            }
-        }
-    )
-    puts response unless response
-  end
-end
-
+require './lib/jira_sender'
 sender = JIRA_SENDER.new
-
 
 # -----------------------------------------------
 # Confluence -> JIRA
@@ -67,11 +26,10 @@ class CONFLUENCE
   CONFLUENCE_PAGE = ENV['CONFLUENCE_HOST'] + 'pages/viewpage.action?pageId=' + ENV['CONFLUENCE_PAGE']
 
   def self.titles
+    confluence_result = []
+
     agent = Mechanize.new
     agent.user_agent = 'Mac Safari'
-
-
-    confluence_result = []
 
     agent.get(ENV['CONFLUENCE_HOST'] + 'login.action') do |page|
       page.form_with(:name => 'loginform') do |form|
