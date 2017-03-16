37766d752598728f8b0c1412fb347b0d4918a41d
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-16 14:40:54 +0900

    get redmine issues

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -17,13 +17,17 @@ class REDMINE
   URL_HOST = "#{ENV['REDMINE_URL']}/projects/#{ENV['REDMINE_PROJECT_NAME']}/issues.json"
   URL_PARAMS = "?key=#{ENV['REDMINE_API_KEY']}&query_id=#{ENV['REDMINE_QUERY_ID']}"
   URL_PREFIX = URL_HOST + URL_PARAMS
+  LIMIT_SIZE = 100
 
   def self.request
-    request = URL_PREFIX + '&limit=1' + '&page=1'
+    pages, issues = call_api['total_count'].to_i / LIMIT_SIZE + 1, {}
+    pages.times { |page| call_api(LIMIT_SIZE, page)['issues'].each { |issue| issues[issue['id']] = issue['subject'] } }
+    issues # => {issue_number: 'issue_subject', ... }
+  end
 
+  def self.call_api(limit=1, page=1)
+    request = "#{URL_PREFIX}&limit=#{limit}&page=#{page}"
     response = open(request, &:read).toutf8
-    parsed = JSON.parse response
-
-    puts parsed
+    JSON.parse(response)
   end
 end
