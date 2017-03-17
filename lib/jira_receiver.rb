77997ef981e363dd59cabc254cc7de324fc4c488
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-17 17:15:16 +0900

    refactor

--- lib/jira_receiver.rb
+++ lib/jira_receiver.rb
@@ -13,10 +13,12 @@ require './lib/jira_common'
 # JIRA -> JIRA
 # -----------------------------------------------
 class JIRA_RECEIVER
+  JIRA_ISSUE_URL_PREFIX = "#{ENV['JIRA_HOST']}#{ENV['JIRA_CONTEXT_PATH']}/browse/"
+
   def self.issues
     client = JIRA::Client.new(JIRA_COMMON::options)
     issues = {}
-    client.Issue.jql(ENV['JIRA_JQL']).each { |issue| issues[issue.summary] = "#{ENV['JIRA_HOST']}#{ENV['JIRA_CONTEXT_PATH']}/browse/#{issue.key}" }
+    client.Issue.jql(ENV['JIRA_JQL']).each { |issue| issues[issue.summary] = JIRA_ISSUE_URL_PREFIX + issue.key }
     issues
   end
 end
