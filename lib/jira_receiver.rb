82920ec5324b557f38519ae6967dea6c1d6237c4
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-17 17:13:55 +0900

    get jira issues url

--- lib/jira_receiver.rb
+++ lib/jira_receiver.rb
@@ -15,7 +15,8 @@ require './lib/jira_common'
 class JIRA_RECEIVER
   def self.issues
     client = JIRA::Client.new(JIRA_COMMON::options)
-    issues = client.Issue.jql(ENV['JIRA_JQL'])
-    puts issues.length
+    issues = {}
+    client.Issue.jql(ENV['JIRA_JQL']).each { |issue| issues[issue.summary] = "#{ENV['JIRA_HOST']}#{ENV['JIRA_CONTEXT_PATH']}/browse/#{issue.key}" }
+    issues
   end
 end
