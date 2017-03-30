600558745bb377d58e5476a93ff338dfc8c519cd
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-30 16:24:22 +0900

    rubocop formatter

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -20,27 +20,30 @@ class REDMINE
   URL_PREFIX = URL_HOST + URL_PARAMS
   LIMIT_SIZE = 100
 
-  def self.issues
-    pages = request['total_count'].to_i / LIMIT_SIZE + 1
-    issues = {}
-    pages.times do |page|
-      this_page = page + 1
-      items = request(LIMIT_SIZE, this_page)['issues']
-      puts "page=#{this_page}/#{pages} : #{items.length} issues found!"
-      items.each { |issue| issues[issue['subject']] = url_of(issue) }
+  class << self
+
+    def issues
+      pages = request['total_count'].to_i / LIMIT_SIZE + 1
+      issues = {}
+      pages.times do |page|
+        this_page = page + 1
+        items = request(LIMIT_SIZE, this_page)['issues']
+        puts "page=#{this_page}/#{pages} : #{items.length} issues found!"
+        items.each { |issue| issues[issue['subject']] = url_of(issue) }
+      end
+      issues # => { 'issue_subject': 'issue_url', ... }
     end
-    issues # => { 'issue_subject': 'issue_url', ... }
-  end
 
-  private
+    private
 
-  def self.request(limit = 1, page = 1)
-    request = "#{URL_PREFIX}&limit=#{limit}&page=#{page}"
-    response = open(request, &:read).toutf8
-    JSON.parse(response)
-  end
+    def request(limit = 1, page = 1)
+      request = "#{URL_PREFIX}&limit=#{limit}&page=#{page}"
+      response = open(request, &:read).toutf8
+      JSON.parse(response)
+    end
 
-  def self.url_of(issue)
-    "#{ENV['REDMINE_URL']}issues/#{issue['id']}"
+    def url_of(issue)
+      "#{ENV['REDMINE_URL']}issues/#{issue['id']}"
+    end
   end
 end
