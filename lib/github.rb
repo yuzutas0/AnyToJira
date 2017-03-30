1e746f04e504678b37e4caa73ea2044a4c6546a8
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-30 16:23:35 +0900

    rubocop formatter

--- lib/github.rb
+++ lib/github.rb
@@ -15,33 +15,36 @@ require 'octokit'
 class GITHUB
   REPOSITORIES = ENV['GITHUB_REPOSITORIES'].split(',')
 
-  def self.issues
-    issues = {}
-    client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-    REPOSITORIES.each { |repo| responses(client, repo).each { |issue| issues[issue.title] = issue.html_url } }
-    issues
-  end
+  class << self
 
-  private
+    def issues
+      issues = {}
+      client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+      REPOSITORIES.each { |repo| responses(client, repo).each { |issue| issues[issue.title] = issue.html_url } }
+      issues
+    end
 
-  def self.responses(client, repository)
-    responses, next_response = call_api(client, repository)
-    paging(responses, next_response)
-    pull_request_prefix = "https://github.com/#{ENV['GITHUB_ORGANIZATION']}/#{repository}/pull/"
-    responses.reject { |response| response.html_url.start_with? pull_request_prefix }
-  end
+    private
 
-  def self.call_api(client, repository)
-    responses = []
-    responses.concat client.issues("#{ENV['GITHUB_ORGANIZATION']}/#{repository}")
-    next_response = client.last_response.rels[:next]
-    [responses, next_response]
-  end
+    def responses(client, repository)
+      responses, next_response = call_api(client, repository)
+      paging(responses, next_response)
+      pull_request_prefix = "https://github.com/#{ENV['GITHUB_ORGANIZATION']}/#{repository}/pull/"
+      responses.reject { |response| response.html_url.start_with? pull_request_prefix }
+    end
 
-  def self.paging(responses, next_response)
-    while next_response
-      responses.concat next_response.get.data
-      next_response = next_response.get.rels[:next]
+    def call_api(client, repository)
+      responses = []
+      responses.concat client.issues("#{ENV['GITHUB_ORGANIZATION']}/#{repository}")
+      next_response = client.last_response.rels[:next]
+      [responses, next_response]
+    end
+
+    def paging(responses, next_response)
+      while next_response
+        responses.concat next_response.get.data
+        next_response = next_response.get.rels[:next]
+      end
     end
   end
 end
