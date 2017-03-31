fb767d6df1a06bf1e57cc66c0037a647b5b07cb9
yuzutas0 <yuzutas0@users.noreply.github.com>
2017-03-31 11:44:26 +0900

    rubocop formatter

--- lib/jira_receiver.rb
+++ lib/jira_receiver.rb
@@ -14,12 +14,12 @@ require './lib/jira_common'
 # JIRA -> JIRA
 # -----------------------------------------------
 class JiraReceiver
-  JIRA_ISSUE_URL_PREFIX = "#{ENV['JIRA_HOST']}#{ENV['JIRA_CONTEXT_PATH']}/browse/"
+  URL_PREFIX = "#{ENV['JIRA_HOST']}#{ENV['JIRA_CONTEXT_PATH']}/browse/"
 
   def self.issues
     client = JIRA::Client.new(JIRA_COMMON.options)
     issues = {}
-    client.Issue.jql(ENV['JIRA_JQL']).each { |issue| issues[issue.summary] = JIRA_ISSUE_URL_PREFIX + issue.key }
+    client.Issue.jql(ENV['JIRA_JQL']).each { |issue| issues[issue.summary] = URL_PREFIX + issue.key }
     issues
   end
 end
